429251639432bf1b9dfaf865eded992b5b5be7fe
franclobo <fjbl2788@gmail.com>
2022-07-15 01:48:38 -0500

    Create components and reducers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography } from 'antd';
+
+import {
+  Homepage, Cryptocurrencies, CryptoDetails, Navbar,
+} from './components';
+import './App.css';
 
 const App = () => (
-  <div>
-    <h1>Cryptoverse</h1>
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+          </Routes>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+          Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link>
+          <br />
+          All Rights Reserved.
+        </Typography.Title>
+      </div>
+    </div>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import generateStore from './redux/configureStore';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import './index.css';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={generateStore()}>
+    <Router>
+      <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );
